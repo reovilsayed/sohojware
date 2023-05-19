d407cd2c1be080bcdee36b91ea7fe514350e3f44
sayed khan <reovilsayed@gmail.com>
2023-05-19 08:37:25 +0600

    services clean up

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -7,10 +7,6 @@ use App\Models\Category;
 use App\Models\Page;
 use App\Models\Portfolio;
 use App\Models\Post;
-use App\Service;
-use Illuminate\Http\Request;
-
-
 class PageController extends Controller
 {
     public function home()
@@ -27,24 +23,6 @@ class PageController extends Controller
         $categories = Category::get();
         return view('posts', compact('posts', 'categories'));
     }
-    public function service($slug)
-    {
-        $service = Category::where('slug', $slug)->firstOrFail();
-        $services = Category::limit(5)->get();
-        return view('service', compact('service', 'services'));
-    }
-    public function portfolios()
-    {
-        $clients = Client::all();
-        $portfolios = Portfolio::latest()->limit(9)->get();
-        return view('portfolios', compact('clients', 'portfolios'));
-    }
-    public function portfolio($slug)
-    {
-        $portfolio = Portfolio::where('slug', $slug)->firstOrFail();
-        return view('single_portfolio', compact('portfolio'));
-    }
-
     public function post($slug)
     {
         $post = Post::where('slug', $slug)->firstOrFail();
@@ -65,6 +43,26 @@ class PageController extends Controller
         $services = Category::latest()->get();
         return view('services', compact('services',));
     }
+    public function service($slug)
+    {
+        $service = Category::where('slug', $slug)->firstOrFail();
+        $services = Category::whereNot('id',$service->id)->get();
+        return view('service', compact('service', 'services'));
+    }
+    public function portfolios()
+    {
+        $clients = Client::all();
+        $portfolios = Portfolio::latest()->limit(9)->get();
+        return view('portfolios', compact('clients', 'portfolios'));
+    }
+    public function portfolio($slug)
+    {
+        $portfolio = Portfolio::where('slug', $slug)->firstOrFail();
+        return view('single_portfolio', compact('portfolio'));
+    }
+
+
+
     public function about()
     {
         return view('about');
