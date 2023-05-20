dd1e562ea15815cd5e4fd52148ecf84905e46208
Abdur Rahman <rahmanabdur64870@gmail.com>
2023-05-20 12:40:03 +0600

    post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -11,6 +11,16 @@ class Post extends  \TCG\Voyager\Models\Post
 
     public function category()
     {
-        return $this->belongsTo(Category::class,'category_id');
+        return $this->belongsTo(Category::class, 'category_id');
+    }
+    function scopeFilter($query, array $filter)
+    {
+        $query->when($filter['category'] ?? false, function ($query, $category) {
+            $query->whereHas('category', function ($query) use ($category) {
+                $query->where('categories.slug', $category);
+            });
+        })->when($filter['search'] ?? false, function ($query, $search) {
+            $query->where('title', 'LIKE', '%' . $search . '%')->orWhere('body', 'LIKE', '%' . $search . '%');
+        });
     }
 }
