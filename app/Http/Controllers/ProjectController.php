d416317019fda2fa7f52d29ced5ce471145f4991
Abdur Rahman <rahmanabdur64870@gmail.com>
2024-05-07 15:37:20 +0600

    a

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Project;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class ProjectController extends Controller
 {
@@ -35,7 +38,41 @@ class ProjectController extends Controller
      */
     public function store(Request $request)
     {
-        //
+     
+        if (!Auth::check()) {
+            $validatedUserData = $request->validate([
+                'name' => 'required|string|max:255',
+                'email' => 'required|string|email|max:255|unique:users',
+            ]);
+
+            $user = User::create([
+                'name' => $validatedUserData['name'],
+                'email' => $validatedUserData['email'],
+                'password' => Hash::make(str::random(8)), 
+            ]);
+
+            Auth::login($user); 
+        } else {
+            $user = Auth::user();
+        }
+
+
+        $validatedProjectData = $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'required|string',
+            'type' => 'required|string|max:255',
+            'project_estd' => 'required|date',
+            'payment_type' => 'required|string|max:255',
+            'budget' => 'required|integer',
+            'status' => 'required|integer',
+            'total_paid' => 'required|integer',
+            'note' => 'nullable|string'
+        ]);
+
+        
+        $project = $user->projects()->create($validatedProjectData);
+
+        return redirect()->route('projects.index')->with('success', 'Project created successfully!');
     }
 
     /**
---
+++ database/migrations/2024_05_07_090725_add_columns_to_users.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('image')->nullable();
+            $table->string('company_name')->nullable();
+            $table->string('designation')->nullable();
+            $table->text('address')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('image');
+            $table->dropColumn('company_name');
+            $table->dropColumn('designation');
+            $table->dropColumn('address');
+        });
+    }
+};
