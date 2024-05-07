061bfc233c9b2a29be1cf57dbe3b54064254877d
Shuvo <102598399+Shuvoakonda@users.noreply.github.com>
2024-05-07 15:38:05 +0600

    asd

---
+++ resources/views/components/letsbuild.blade.php
@@ -0,0 +1,21 @@
+<section class="lets-builds  mega-section wow fadeInUp" id="lets-builds" data-wow-delay="0.2s">
+    <div class="container">
+        <section class="locations-section  mega-section ">
+            <div class="sec-heading centered  ">
+                <div class="content-area">
+                    <h2 class=" title    wow fadeInUp" data-wow-delay=".4s">Let's build together</h2>
+                </div>
+            </div>
+            <div class=" contact-info-panel ">
+                <div class="info-section ">
+                    <div class="row">
+                        <div class="col-12 col-lg-4">
+                           
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+</section>
