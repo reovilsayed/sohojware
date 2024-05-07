fab8f3b194df34292fe10231489ad3e8f00cef7b
Abdur Rahman <rahmanabdur64870@gmail.com>
2024-05-07 14:56:30 +0600

    a

---
+++ database/migrations/2024_05_07_075952_create_projects_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('title');
+            $table->text('description');
+            $table->string('type');
+            $table->timestamp('project_estd');
+            $table->string('payment_type');
+            $table->integer('budget')->default(0);
+            $table->tinyInteger('status')->default(0);
+            $table->integer('total_paid')->default(0);
+            $table->text('note')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+};
