830c7c4a0ae51aec8206900418e48ea7cb8e2c11
Shuvo <102598399+Shuvoakonda@users.noreply.github.com>
2024-05-13 01:47:20 +0600

    asd

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-@section('title','Posts' )
+@section('title','Blogs' )
 @section('social_media')
     <meta property="og:title" content="{{ setting('site.title') }}" />
     <meta property="og:description"content="{{ setting('site.description') }}" />
