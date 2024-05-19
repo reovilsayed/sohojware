5e808dc0095d71345fbc574b2de1f18ffbc795b8
shuvoakonda <shuvoakonda429@gmail.com>
2024-05-19 08:04:31 +0200

    done

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -366,6 +366,18 @@
 
         });
     </script>
+    <script async type="application/javascript"
+        src="https://news.google.com/swg/js/v1/swg-basic.js"></script>
+<script>
+  (self.SWG_BASIC = self.SWG_BASIC || []).push( basicSubscriptions => {
+    basicSubscriptions.init({
+      type: "NewsArticle",
+      isPartOfType: ["Product"],
+      isPartOfProductId: "CAow5bquDA:openaccess",
+      clientOptions: { theme: "light", lang: "en" },
+    });
+  });
+</script>
     @yield('js')
 
 </body>
