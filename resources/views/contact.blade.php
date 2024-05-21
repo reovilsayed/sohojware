a7e4514b5ad704c47dc0f2f31b96f85672db4d01
Shuvo <102598399+Shuvoakonda@users.noreply.github.com>
2024-05-21 18:38:39 +0600

    contact address

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -21,6 +21,7 @@
     </x-breadcrumb>
     <section class="contact-us  mega-section  pb-0" id="contact-us">
         <div class="container">
+            <x-contact :doNotShowOnTheseRoutes="[route('login'), route('register')]" />
             <section class="locations-section  mega-section ">
                 <div class="sec-heading centered  ">
                     <div class="content-area">
@@ -30,32 +31,38 @@
                 <div class=" contact-info-panel ">
                     <div class="info-section ">
                         <div class="row">
-                            <div class="col-12 col-lg-4">
+                            <div class="col-12 col-lg-3">
                                 <div class="info-panel wow fadeInUp h-100" data-wow-delay=".4s ">
                                     <h4 class="location-title">United States</h4>
                                     <div class="line-on-side"> </div>
                                     <p class="location-address">Warren st, Hudson, NY 12534.</p>
                                 </div>
                             </div>
-                            <div class="col-12 col-lg-4">
+                            <div class="col-12 col-lg-3">
                                 <div class="info-panel wow fadeInUp h-100" data-wow-delay=".6s">
                                     <h4 class="location-title">Canada</h4>
                                     <div class="line-on-side "> </div>
                                     <p class="location-address">88 McGill Blvd W, Lethbridge, Alberta, CA</p>
                                 </div>
                             </div>
-                            <div class="col-12 col-lg-4">
+                            <div class="col-12 col-lg-3">
                                 <div class="info-panel  wow fadeInUp h-100" data-wow-delay=".8s">
                                     <h4 class="location-title">Asia</h4>
                                     <div class="line-on-side "> </div>
                                     <p class="location-address">1215 Farmgate Dhaka, Bangladesh</p>
                                 </div>
                             </div>
+                            <div class="col-12 col-lg-3">
+                                <div class="info-panel  wow fadeInUp h-100" data-wow-delay=".8s">
+                                    <h4 class="location-title">Bangladesh</h4>
+                                    <div class="line-on-side "> </div>
+                                    <p class="location-address">8200 Notullahbath Barisal, Bangladesh</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </section>
-            <x-contact :doNotShowOnTheseRoutes="[route('login'), route('register')]" />
         </div>
     </section>
 @endsection
