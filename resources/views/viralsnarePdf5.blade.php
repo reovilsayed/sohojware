fbe2ece27886b7ab8717639eba0f688b679a0c7e
Abdur Rahman <rahmanabdur64870@gmail.com>
2024-09-28 14:40:55 +0600

    sad

--- resources/views/viralsnarePdf5.blade.php
+++ resources/views/viralsnarePdf5.blade.php
@@ -140,7 +140,7 @@
     <div class="containar" style="width: 800px; background-color: white">
         <section class="header" style="height: 80px;">
             <div class="logo">
-                <img src={{ asset('assets/157.png') }}" alt="">
+                <img src="{{ asset('assets/157.png') }}" alt="">
             </div>
             <div class="title">
                 <p>Exclusive Content Submission</p>
